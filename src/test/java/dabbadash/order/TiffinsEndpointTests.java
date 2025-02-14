af91c6eeb7c904d3b78123601258e9a0e9278178
arjav448 <arjavgandhi2104@gmail.com>
2025-02-15 00:52:13 +0530

    Updated TiffinEndPoints

--- src/test/java/dabbadash/order/TiffinsEndpointTests.java
+++ src/test/java/dabbadash/order/TiffinsEndpointTests.java
@@ -2,6 +2,7 @@ package dabbadash.order;
 
 import dabbadash.order.entity.Tiffin;
 import dabbadash.order.entity.User;
+import dabbadash.order.repository.TiffinRepository;
 import dabbadash.order.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +27,8 @@ public class TiffinsEndpointTests {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TiffinRepository tiffinRepository;
 
     @Test
     void shouldReturnTiffinsForValidRestaurantId() {
@@ -161,5 +165,4 @@ public class TiffinsEndpointTests {
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         Assertions.assertThat(response.getBody()).contains("Tiffin price is required and it must be positive number");
     }
-
 }
